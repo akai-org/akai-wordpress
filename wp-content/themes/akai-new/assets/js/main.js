ccad2379e26407666a2f9cb78d9226e520766a4e
Jacek Tomaszewski <jacek@jtom.me>
2014-06-28 16:20:45 +0200

    Change .entry-content ol look

--- wp-content/themes/akai-new/assets/js/main.js
+++ wp-content/themes/akai-new/assets/js/main.js
@@ -1,4 +1,4 @@
-/*! AKAI New - v0.1.0 - 2014-06-03
+/*! AKAI New - v0.1.0 - 2014-06-28
  * http://akai.org.pl
  * Copyright (c) 2014; * Licensed GPLv2+ */
 // See http://www.advancedcustomfields.com/resources/field-types/google-map/ for more info.

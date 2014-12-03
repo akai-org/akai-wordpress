f69c74fd707fa1c2c3c4d709cfe2f9a765988a87
Jacek Tomaszewski <jacek@jtom.me>
2014-12-03 17:34:09 +0100

    Fix add_to_calendar link (it was 1h wrong because of the DST)

--- wp-content/themes/akai-new/includes/template_tags.php
+++ wp-content/themes/akai-new/includes/template_tags.php
@@ -4,10 +4,10 @@
  * prints Google Add to calendar link
  * more info: http://stackoverflow.com/questions/10488831/link-to-add-to-google-calendar
  * @param int $duration in seconds
- * 
+ *
  * @todo czy to ma byc link Add to calendar, czy moze jakis obiekt, ktory mozna dodawac takze do innych kalendarzy?
- *   np. plik .iCal 
- *   http://www.phpclasses.org/package/873-PHP-Class-for-creating-iCalendar-files.html 
+ *   np. plik .iCal
+ *   http://www.phpclasses.org/package/873-PHP-Class-for-creating-iCalendar-files.html
  *   http://stackoverflow.com/questions/1661478/add-an-outlook-calendar-event-from-a-link-on-a-web-page
  *   http://www.myhow2guru.com/archives/php-generate-calendar-file-ics/
  *   lub plik .vcs od outlooka (buehehehe; u're kidding right?) http://www.terminally-incoherent.com/blog/2008/04/14/generate-outlook-calendar-events-with-php-and-icalendar/
@@ -21,9 +21,9 @@ function akai_add_to_calendar_url($duration = 5400) {
   // Stupid hack to fix time set on Google Calendar.
   // Even if you pass the 18:30 time to Google and a timezone, it will convert the time to its current timezone anyway (thus, making 20:30 from it, if we're in Warsaw (2 hours between UTC and Europe/Warsaw))
   $hours_offset = 2;
-  if (date_i18n('I')) {
+  // if (date_i18n('I')) {
     $hours_offset -= 1;
-  }
+  // }
   $start_timestamp -= $hours_offset * 60 * 60;
 
   $end_timestamp = $start_timestamp + $duration;
